f5a96bbe1ae66770d6d9d6a0f539b92b4064463a
Muhammad Farid Zia <muhfaridzia@pahamify.com>
2021-11-11 21:49:03 +0800

    feat: add new short articles

---
+++ posts/article/penat.md
@@ -0,0 +1,9 @@
+---
+title: Penat
+slug: penat
+date: 'November 11, 2021'
+category: 'Life'
+short_description: Penat!
+---
+
+PENAT!!!!!!!!!!!!!!!!!!
--- posts/index.js
+++ posts/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 export default [
+  'penat',
   'belajar-googling-untuk-pemrogram',
   'balik-kampung-disaat-wabah-covid-19',
   'mimpimu-mimpiku',
